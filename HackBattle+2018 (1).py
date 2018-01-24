c2a556f791d638b31f43c9cebb12d4f30c58fdb2
saiprathyusha97 <mukkusaiprathyusha@gmail.com>
2018-01-25 03:05:07 +0530

    confusion matrix in scikit
    A confusion matrix shows the proper labels on the main diagonal .
    here we used tfidf vector

--- HackBattle+2018 (1).py
+++ HackBattle+2018 (1).py
@@ -42,3 +42,54 @@ difference = set(count_df.columns) - set(tfidf_df.columns)
 print(count_df.equals(tfidf_df))
 count_df.head()
 
+
+# In[6]:
+
+
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+    
+
+
+# In[7]:
+
+
+clf = MultinomialNB()
+clf.fit(tfidf_train, y_train)
+pred = clf.predict(tfidf_test)
+score = accuracy_score(y_test, pred)
+print("accuracy:   %0.3f" % score)
+cm = confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
+plot_confusion_matrix(cm, classes=['FAKE', 'REAL'])
+
