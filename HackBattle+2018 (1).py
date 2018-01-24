c027abf88547ecd4b92d9820df3cd2eb16e90e2b
saiprathyusha97 <mukkusaiprathyusha@gmail.com>
2018-01-25 02:48:46 +0530

    TFIDF features
    Convert a collection of raw documents to a matrix of TF-IDF features

--- HackBattle+2018 (1).py
+++ HackBattle+2018 (1).py
@@ -12,12 +12,13 @@ y = df.label
 df.drop("label", axis=1)
 
 
-# In[3]:
+# In[4]:
 
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 df = pd.read_csv("fake_or_real_news.csv")
 df.shapedf = df.set_index("Unnamed: 0")
 df.head()
@@ -27,5 +28,7 @@ X_train, X_test, y_train, y_test = train_test_split(df['text'], y, test_size=0.6
 count_vectorizer = CountVectorizer(stop_words='english')
 count_train = count_vectorizer.fit_transform(X_train)
 count_test = count_vectorizer.transform(X_test)
-count_vectorizer
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+tfidf_train = tfidf_vectorizer.fit_transform(X_train) 
+tfidf_test = tfidf_vectorizer.transform(X_test)
 
